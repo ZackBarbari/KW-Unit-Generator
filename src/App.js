23e09bf46a2bd781e6f8d17ab9929fdc3fae1747
JuanPablo2655 <personal@isidrocasiano.com>
2022-12-11 20:07:35 -0500

    refactor/css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import InputBox from "./components/InputBox";
-import UnitCard from "./components/UnitCard";
-import { useState } from "react";
+import InputBox from './components/InputBox';
+import UnitCard from './components/UnitCard';
+import { useState } from 'react';
 
 const App = () => {
-  const [body, setBody] = useState();
+	const [body, setBody] = useState();
 
-  const modifyCard = (body) => {
-    console.log("type: " + body.unit)
-    setBody(body)
-  }
+	const modifyCard = body => {
+		console.log('type: ' + body.unit);
+		setBody(body);
+	};
 
-  return (
-    <>
-    <div className="App">
-      <h1>Kingdoms and Warfare</h1>
-      <h2>Unit Card Generator</h2>
-      <InputBox onMod={modifyCard}/>
-      <UnitCard {...body} />
-    </div>
-    </>
-  );
-}
+	return (
+		<>
+			<div className='App'>
+				<header>
+					<h1>Kingdoms and Warfare</h1>
+					<h2>Unit Card Generator</h2>
+				</header>
+				<main>
+					<InputBox onMod={modifyCard} />
+					<UnitCard {...body} />
+				</main>
+			</div>
+		</>
+	);
+};
 
 export default App;
