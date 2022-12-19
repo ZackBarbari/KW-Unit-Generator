9a806834635d1bfabcc6a7937a84b48373ee9ec9
Zack Barbari <zackb@unc.edu>
2022-12-18 23:48:32 -0500

    Display bug fixed

--- src/components/UnitCard.js
+++ src/components/UnitCard.js
@@ -3,6 +3,7 @@ import defaults from '../constants/statics/defaults';
 import React from 'react';
 const UnitCard = React.forwardRef(
     ({name, commander, ancestry, unit, exp, equip, race, size, tier, traits}, ref) => {
+        UnitCard.defaultProps = defaults
 
     //ref={ref}console.log(name, commander, ancestry, unit, exp, equip, race, size, traits, ref)
 
@@ -42,6 +43,5 @@ const UnitCard = React.forwardRef(
             <img src={ require(`../symbols/type/${unit}.png`)} alt="Type Icon" className='type-icon'></img>
             {race}<br />
             {traits.map((trait) => (<div>{trait.name} {trait.description}</div>))}*/
-UnitCard.defaultProps = defaults
 
 export default UnitCard;
