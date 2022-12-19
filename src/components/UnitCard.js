538a20bc882cece82c7862a7ddef7bd814ca3689
Zack Barbari <zackb@unc.edu>
2022-12-18 22:41:43 -0500

    Card redone, flex box zones made

--- src/components/UnitCard.js
+++ src/components/UnitCard.js
@@ -9,24 +9,29 @@ const UnitCard = React.forwardRef(
     return (
         <>
         <div className='card-outline' ref={ref}>
-            <img src={ require(`../symbols/type/${unit}.png`)} alt="Type Icon" width="140" height="140"></img>
-            <div className='size-rhombus'></div>
-            <div className='tier-rhombus'></div>
-            <img src={ require(`../symbols/ancestry/${ancestry}.png`)} alt="Ancestry Icon" className='ancestry-icon'></img><br />
-            {size}<br />
-            {name}<br />
-            Commander {commander}<br />
-            {ancestry}<br />
-            {exp}<br />
-            {equip}<br />
-            {race}<br />
-            {traits.map((trait) => (<div>{trait.name} {trait.description}</div>))}
+            <div className='first-container'>
+                <div className='info-container'>
+                    <div className='name-container' />
+                    <div className='trait-container' />
+                </div>
+                <div className='rhombus-container'/>
+            </div>
+            <div className='stats-container' />
         </div>
         </>
     )
 }
 );
 
+/*
+<img src={ require(`../symbols/ancestry/${ancestry}.png`)} alt="Ancestry Icon" className='ancestry-icon'></img><br />
+            {name}<br />
+            Commander {commander}<br />
+            {exp}<br />
+            {equip}<br />
+            <img src={ require(`../symbols/type/${unit}.png`)} alt="Type Icon" className='type-icon'></img>
+            {race}<br />
+            {traits.map((trait) => (<div>{trait.name} {trait.description}</div>))}*/
 UnitCard.defaultProps = defaults
 
 export default UnitCard;
